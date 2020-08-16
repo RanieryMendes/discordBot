7bf7f68eab2abb7b8311d4e8a11acf3a9ec4c1e3
RanieryMendes <mendrc18@wfu.edu>
2020-08-16 01:28:08 -0400

    Invitation skills and welcome message for new users added

--- discBot.py
+++ discBot.py
@@ -18,6 +18,7 @@ bot.footer= "Investe comigo"
 bot.footer_image= "https://png.pngtree.com/png-clipart/20190612/original/pngtree-a-wad-of-dollar-bills-png-image_3326543.jpg"
 @bot.event
 
+
 async def on_ready():
     print("I'm connected to Discord")
 
@@ -38,6 +39,37 @@ async def on_ready():
     bot.log_channel = bot.get_channel(670030020621762610)
     await bot.log_channel.send(embed=embed)
     print(f"Message sent on Discord channel. My ID is {bot.user.id}")
+
+
+
+@bot.event
+async def on_member_join(member):
+
+    print("calling on join")
+    embed = discord.Embed(
+        title=f"{member.display_name} chegou! Ele ta aqui desde{member.joined_at()}",
+        color= bot.embed_color,
+        timestamp= datetime.datetime.now(datetime.timezone.utc)
+    )
+
+    embed.set_footer(
+
+        text=bot.footer,
+        icon_url=bot.footer_image
+    )
+
+   # mensagem = Message(
+       # content=f"{member.display_name} chegou! Ele ta aqui desde{member.joined_at()}",
+        #created_at=datetime.datetime.now(datetime.timezone.utc),
+    #)
+    for channel in member.guild.channels:
+            await channel.send_message(f"Welcome {member.display_name}")
+    
+    print("calling on join2")
+    bot.log_channel = bot.get_channel(670030020621762610)
+    await bot.log_channel.send(embed=embed)
+ 
+
 
 @bot.command(
     name="restart",
@@ -63,11 +95,52 @@ async def restart(ctx):
         icon_url=bot.footer_image
     )
 
+    
     await bot.log_channel.send(embed=embed)
 
     await ctx.message.add_reaction('☑️')
 
     await bot.close()
+
+
+@bot.command(
+    name="invite",
+    aliases=["i"],
+    help="It creates a link invitation to the server"
+) 
+
+async def invite(ctx):
+
+    embed = discord.Embed(
+        title=f" Toma aqui a porra do convite, {ctx.author.name}!",
+        color= bot.embed_color,
+        timestamp= datetime.datetime.now(datetime.timezone.utc)
+    )
+
+    embed.set_author(
+        name=ctx.author.name,
+        icon_url=ctx.author.avatar_url
+
+    )
+
+    embed.set_footer(
+
+        text=bot.footer,
+        icon_url=bot.footer_image
+    )
+
+    invitation = await ctx.channel.create_invite(max_uses=1,unique=True,max_age=120)
+
+
+    
+    
+    await bot.log_channel.send(embed=embed, content=invitation)
+
+    await ctx.author.send(invitation)
+
+
+
+
 
 bot.run(token, bot=True, reconnect=True)
 
